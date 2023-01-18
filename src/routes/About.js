3491ffea587c755188d5e42248c56c9c8fdb24b3
lee minho <twomean99@gmail.com>
2023-01-19 00:10:58 +0900

    디테일화면 완료

---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./About.css";
+
+function About(props) {
+  console.log(props);
+  return <span>about this page</span>;
+}
+
+export default About;
---
+++ src/routes/Detail.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Detail.css";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    } //클릭하지않고 url로 이동했을 때 홈페이지로 리다이렉트
+  }
+  render() {
+    const { location } = this.props;
+    return (
+      <div className="detail_movie">
+        <img
+          src={location.state.poster}
+          alt={location.state.title}
+          title={location.state.title}
+        />
+        <div className="detail_text">
+          <h1>{location.state.title}</h1>
+          <h5 className="movie__year">{location.state.year}</h5>
+          <ul className="genres">
+            {location.state.genres.map((genres, index) => (
+              <li key={index} className="genres__genres">
+                {genres}
+              </li>
+            ))}
+          </ul>
+          <p className="movie-summary">
+            {location.state.summary.slice(0, 180)}...
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Detail;
